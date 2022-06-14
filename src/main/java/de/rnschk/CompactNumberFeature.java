eb7d3e8264d3d0c8f5002ef11e889c6efdf63865
rnschk <rnschk@gmail.com>
2022-06-14 16:01:23 +0200

    add some numbers

--- src/main/java/de/rnschk/CompactNumberFeature.java
+++ src/main/java/de/rnschk/CompactNumberFeature.java
@@ -7,9 +7,15 @@ public class CompactNumberFeature {
 
     public static void main(final String[] args) {
         var englishShort = NumberFormat.getCompactNumberInstance(Locale.ENGLISH, NumberFormat.Style.SHORT);
-        System.out.println(englishShort.format(1000));
-        System.out.println(englishShort.format(100000));
-        System.out.println(englishShort.format(1000000));
+        System.out.println(englishShort.format(499)); // 499
+        System.out.println(englishShort.format(500)); // 500
+        System.out.println(englishShort.format(501)); // 501
+        System.out.println(englishShort.format(1000)); // 1K
+        System.out.println(englishShort.format(100000)); // 100K
+        System.out.println(englishShort.format(1000000)); // 1M
+        System.out.println(englishShort.format(1499999)); // 1M
+        System.out.println(englishShort.format(1500000)); // 2M
+        System.out.println(englishShort.format(1500001)); // 2M
         System.out.println("---");
 
         var englishLong = NumberFormat.getCompactNumberInstance(Locale.ENGLISH, NumberFormat.Style.LONG);
